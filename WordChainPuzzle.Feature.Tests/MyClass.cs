43807d796704b78db8ec749cea36096714a1b06c
aegraham <22817954+aegraham@users.noreply.github.com>
2022-01-10 20:05:52 +0000

    Initial Commit
    Added simple MVC application.
    With a feature project, which includes a file manager.
    Create a Test project for the Features. Loaded up the dictionary file. Test to see if it loads and test to make sure all the values are loaded

---
+++ WordChainPuzzle.Feature.Tests/MyClass.cs
@@ -0,0 +1,32 @@
+﻿
+using NUnit.Framework;
+using WordChainPuzzle.Feature.Words;
+
+namespace WordChainPuzzle.Feature.Tests
+{
+    [TestFixture]
+    public class FileManagerTests
+    {
+        public FileManagerTests()
+        {
+        }
+
+        [Test]
+        public void TestLoadFile()
+        {
+            var fileManager = new FileManager();
+            fileManager.LoadWords("files/dictionary.txt");
+            Assert.IsNotNull(fileManager.Words);
+        }
+
+        [Test]
+        public void TestLoadFile_With_45425Words()
+        {
+            var fileManager = new FileManager();
+            fileManager.LoadWords("files/dictionary.txt");
+            Assert.AreEqual(fileManager.Words.Count, 45425);
+        }
+
+
+    }
+}
---
+++ WordChainPuzzle/Models/HomeModel.cs
@@ -0,0 +1,16 @@
+﻿namespace WordChainPuzzle.Models
+{
+    public class HomeModel
+    {
+        public HomeModel()
+        {
+            
+        }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string FirstWord { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Required]
+        public string SecondWord { get; set; }
+    }
+}
