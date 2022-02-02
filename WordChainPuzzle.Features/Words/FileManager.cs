794af98f4a62ce4aced17b74b439d23b87927b2e
aegraham <22817954+aegraham@users.noreply.github.com>
2022-02-02 08:17:05 +0000

    Removed the MVC app added a console app

---
+++ WordChainPuzzle.Features/Words/FileManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WordChainPuzzle.Feature.Words
+{
+    public class FileManager
+    {
+        public List<string> Words { get; set; }
+
+        public FileManager()
+        {
+        }
+
+        public void LoadWords(string filePath)
+        {
+            //TODO May be a better way to find the directory path than this.
+            var directory = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"../../../../"));
+            var file = File.ReadAllLines(directory + "/" + filePath);
+            Words = new List<string>(file);
+        }
+    }
+}
---
+++ WordPuzzleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using WordChainPuzzle.Feature.Words;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to word chain.");
+            Console.WriteLine("Please enter your first word");
+            var firstWord = Console.ReadLine();
+            Console.WriteLine("Please enter your second word");
+            var lastWord = Console.ReadLine();
+
+            if (firstWord.Length != lastWord.Length)
+            {
+                Console.WriteLine("Words do not match.");
+            }
+            
+            var wordEngine = new WordEngine("../files/dictionary.txt");
+            var startTime = DateTime.Now;
+            var words = wordEngine.GetWordChain(firstWord, lastWord);
+            var stopTime = DateTime.Now;
+            Console.WriteLine(words + " time taken = " + (stopTime.TimeOfDay - startTime.TimeOfDay));
+        }
+        
+      
+    }
+}
